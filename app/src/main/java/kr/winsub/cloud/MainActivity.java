bf943c6f207427aa75415cd67d14c64c35ce11e2
kongwoojin <kongwoojin03@gmail.com>
2019-05-26 16:55:15 +0900

    Optimize import

--- app/src/main/java/kr/winsub/cloud/MainActivity.java
+++ app/src/main/java/kr/winsub/cloud/MainActivity.java
@@ -1,11 +1,9 @@
 package kr.winsub.cloud;
 
-import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
